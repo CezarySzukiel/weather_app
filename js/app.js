1068f28c84a670944717242478b36c59604a10b1
Cezary <cezary.szukiel@gmail.com>
2023-08-06 23:01:29 +0200

    Refactored city search form visibility toggle.

--- js/app.js
+++ js/app.js
@@ -3,10 +3,22 @@ const city = 'auto:ip'
 const days = 5
 
 class Weather {
+     /**
+     * Creates an instance of the Weather class.
+     * @constructor
+     * @param {string} city - City name for which weather data is retrieved.
+     * @param {number} days - Number of days of weather forecast.
+     */
     constructor(city, days) {
         this.city = city
         this.days = days
     }
+
+     /**
+     * Gets weather data from an external API.
+     * @async
+     * @returns {Promise} - An object containing weather data.
+     */
     async getWeather() {
         try {
             const response = await fetch(
@@ -23,24 +35,52 @@ class Weather {
 }
 
 class FormDataHandler {
+    /**
+     * Creates an instance of the FormDataHandler class.
+     * @param form
+     */
     constructor(form) {
         this.form = form
     }
+
+    /**
+     * Gets data from the form.
+     * @param search
+     * @returns {FormDataEntryValue}
+     */
     getFormData(search) {
         const formData = new FormData(this.form)
         return formData.get(search)
+        // todo walidacja danych, aby nie zawierały polskich znaków
     }
 }
 
-// show/hide city searching form
+// kod który wykona się po załadowaniu strony i pobierze dane pogodowe dla aktualnej lokalizacji
+/**
+ * Gets weather data for the current location.
+ * @type {Promise}
+ */
+const weather = new Weather(city, days).getWeather()
+console.log(weather)
+
+/**
+ * Toggles the visibility of the city searching form.
+ */
+function setupToggleVisibilityElem(triggerElement, targetElement) {
+    triggerElement.addEventListener('click', function () {
+        targetElement.toggleAttribute('hidden')
+    })
+}
+const formDiv = document.querySelector('.module__form')
 const addCityButton = document.getElementById('add-city')
-addCityButton.addEventListener('click', function () {
-    const formDiv = document.querySelector('.module__form')
-    formDiv.toggleAttribute('hidden')
-})
+const closeButton = document.querySelector('.btn--close')
+setupToggleVisibilityElem(addCityButton, formDiv)
+setupToggleVisibilityElem(closeButton, formDiv)
 
 
-
+/**
+ * Handles the form submission event.
+ */
 document.querySelector('.find-city').addEventListener('submit', function (event) {
     event.preventDefault();
     const formData = new FormDataHandler(this);
@@ -48,6 +88,33 @@ document.querySelector('.find-city').addEventListener('submit', function (event)
     console.log(searchValue)
     const weather = new Weather(searchValue, days).getWeather()
     console.log(weather)
-    // todo walidacja danych, aby nie zawierały polskich znaków
+
 })
 
+
+// funkcja, która z pobranych danych z API wyświetli je na stronie
+
+/**
+ * Displays weather data on the page.
+ */
+function displayWeather(city, temperature, pressure, humidity, wind) {
+    const weatherElement = document.querySelector('.module__weather')
+    weatherElement.removeAttribute('hidden')
+    let cityName = weatherElement.querySelector('.city__name')
+    let cityTemperature = weatherElement.querySelector('.temperature')
+    let cityPressure = weatherElement.querySelector('.pressure__value')
+    let cityHumidity = weatherElement.querySelector('.humidity__value')
+    let cityWind = weatherElement.querySelector('.wind-speed__value')
+    cityName.textContent = city
+    cityTemperature.textContent = temperature
+    cityPressure.textContent = pressure
+    cityHumidity.textContent = humidity
+    cityWind.textContent = wind
+
+
+
+    console.log(cityName)
+}
+let wachock = 'Wachock'
+
+displayWeather(wachock)
