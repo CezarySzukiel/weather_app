b7e2941a39bf9f734e898211c6df1479b9413e27
Cezary <cezary.szukiel@gmail.com>
2023-08-07 11:07:05 +0200

    Shows current weather on page

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ const apiKey = '0a573a049e12438cac9101900230608'
 const city = 'auto:ip'
 const days = 5
 
+
 class Weather {
+    url = 'http://api.weatherapi.com/v1/forecast.json'
      /**
      * Creates an instance of the Weather class.
      * @constructor
@@ -19,19 +21,52 @@ class Weather {
      * @async
      * @returns {Promise} - An object containing weather data.
      */
-    async getWeather() {
+    async getWeatherFromApi() {
         try {
             const response = await fetch(
-                `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${this.city}&days=${this.days}`
+                `${this.url}?key=${apiKey}&q=${this.city}&days=${this.days}`
             )
-                const data = await response.json()
-            console.log(data)
+            return await response.json()
         }
         catch (error) {
-            console.log(error);
+            console.error(error);
             throw error;
         }
     }
+
+    /**
+     * Gets needed weather data from the API and displays it on the page.
+     * @returns {Promise<void>}
+     */
+    async getWeatherData () {
+        const weatherData = await this.getWeatherFromApi()
+        const cityName = weatherData.location.name
+        const temperature = weatherData.current.temp_c
+        const humidity = weatherData.current.humidity
+        const pressure = weatherData.current.pressure_mb
+        const wind = weatherData.current.wind_kph
+        this.displayWeather(cityName, temperature, pressure, humidity, wind)
+    }
+
+    /**
+     * Displays weather data on the page.
+     * @param city
+     * @param temperature
+     * @param pressure
+     * @param humidity
+     * @param wind
+     */
+    displayWeather(city, temperature, pressure, humidity, wind) {
+        const weatherElement = document.querySelector('.module__weather')
+        weatherElement.removeAttribute('hidden')
+        weatherElement.querySelector('.city__name').textContent = city
+        weatherElement.querySelector('.temperature').textContent = temperature
+        weatherElement.querySelector('.pressure__value').textContent = pressure
+        weatherElement.querySelector('.humidity__value').textContent = humidity
+        weatherElement.querySelector('.wind-speed__value').textContent = wind
+        console.log('city, temp, press, hum, wind: ', city, temperature, pressure, humidity, wind)
+}
+
 }
 
 class FormDataHandler {
@@ -55,13 +90,15 @@ class FormDataHandler {
     }
 }
 
-// kod który wykona się po załadowaniu strony i pobierze dane pogodowe dla aktualnej lokalizacji
 /**
- * Gets weather data for the current location.
+ * Gets weather data for the current location after page load.
  * @type {Promise}
  */
-const weather = new Weather(city, days).getWeather()
-console.log(weather)
+const weather = new Weather(city, days).getWeatherData()
+
+// sprawdzam jak wyglądają dane z api:
+const weatherData = new Weather(city, days).getWeatherFromApi()
+console.log('dane z api:', weatherData)
 
 /**
  * Toggles the visibility of the city searching form.
@@ -86,35 +123,8 @@ document.querySelector('.find-city').addEventListener('submit', function (event)
     const formData = new FormDataHandler(this);
     const searchValue = formData.getFormData('search');
     console.log(searchValue)
-    const weather = new Weather(searchValue, days).getWeather()
+    const weather = new Weather(searchValue, days).getWeatherData()
     console.log(weather)
-
 })
 
 
-// funkcja, która z pobranych danych z API wyświetli je na stronie
-
-/**
- * Displays weather data on the page.
- */
-function displayWeather(city, temperature, pressure, humidity, wind) {
-    const weatherElement = document.querySelector('.module__weather')
-    weatherElement.removeAttribute('hidden')
-    let cityName = weatherElement.querySelector('.city__name')
-    let cityTemperature = weatherElement.querySelector('.temperature')
-    let cityPressure = weatherElement.querySelector('.pressure__value')
-    let cityHumidity = weatherElement.querySelector('.humidity__value')
-    let cityWind = weatherElement.querySelector('.wind-speed__value')
-    cityName.textContent = city
-    cityTemperature.textContent = temperature
-    cityPressure.textContent = pressure
-    cityHumidity.textContent = humidity
-    cityWind.textContent = wind
-
-
-
-    console.log(cityName)
-}
-let wachock = 'Wachock'
-
-displayWeather(wachock)
